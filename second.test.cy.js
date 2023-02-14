b8c87cbb62bf66d9c5645521af54077d973ad7c5
asyiddiki <hasbi3108@gmail.com>
2023-02-14 21:13:19 +0700

    kumpul tugas

---
+++ second.test.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+  it("click 'type' shows the right headings", () => {
+    cy.visit("https://example.cypress.io");
+    //cy.pause() 
+    cy.contains('type').click()
+    
+    // Should be a new URl which includes '/comment/action' 
+    cy.url(). should('includes', '/commands/actions')
+    
+    // Get input, type into it and verify that the value has been updated
+    cy.get('.action-email') 
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com') 
+  })
+});
