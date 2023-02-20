99a08bf5bb1694bbbb163d84f3bb4abce91bfc92
asyiddiki <hasbi3108@gmail.com>
2023-02-20 11:28:43 +0700

    kumpul tugas

---
+++ cypress/Custom.commands.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('Working with inputs', () => {
+    it('Visit the website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html');
+        cy.url().should('include', 'login.html');
+      });
+    it('Should try to login', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html');
+        cy.fixture("user").then(user => {
+        const username = user.username
+        const password = user.password
+        cy.login(username, password)
+        cy.get('.alert-error').should('contain.text', 'Login and/or password are wrong.')
+        })
+       
+    });
+});
